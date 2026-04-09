fcf5be5407d302603ff4735c88fca088b8739a36
Ubuntu <azureuser@opentestclaw.lv55ekzducoedibotpflzu3adc.ux.internal.cloudapp.net>
2026-04-09 06:40:09 +0000

    Add NEXT_PUBLIC_BACKEND_URL env fallback in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   /* config options here */
+};
+
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://saas-training-backend.onrender.com',
+  },
 };
 
 export default nextConfig;
